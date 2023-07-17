618ae86be2c58d8f343ee587dc0f1d8011bc026a
seking <1741092821@qq.com>
2023-07-17 22:48:36 +0800

    1

---
+++ androidcreat/app/src/main/java/com/example/creat/firstActivity.java
@@ -0,0 +1,14 @@
+package com.example.creat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class firstActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.firstlayout);
+    }
+}
